f955d5d6a80b39ccfe638f6b579fecd477f37d2f
Aryankishor <73214720+Aryankishor@users.noreply.github.com>
2020-11-09 14:45:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+menu_list_array=["chicken tandoori pizza",
+                 "Veg supreme pizza",
+                 "paneer tikka pizza",
+                 "Deluxe veggie pizza",
+                 "veg extravanga pizza"];
+
+
+
+function getmenu(){
+    var htmldata;
+     htmldata="ol class= 'menulist'>"
+     menu_list_array.sort();
+     
+}
